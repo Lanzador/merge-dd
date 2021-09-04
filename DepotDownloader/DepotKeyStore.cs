ee754425c7637d178d698f537c21d14c587af969
Lanzador <89643951+Lanzador@users.noreply.github.com>
2021-09-04 17:20:21 +0300

    Add files via upload

---
+++ DepotDownloader/DepotKeyStore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DepotDownloader
+{
+    static class DepotKeyStore
+    {
+        private static Dictionary<uint, byte[]> depotKeysCache = new Dictionary<uint, byte[]>();
+
+        public static void AddAll(string[] values)
+        {
+            foreach(string value in values)
+            {
+                string[] split = value.Split(';');
+
+                if(split.Length != 2)
+                {
+                    throw new FormatException($"Invalid depot key line: {value}");
+                }
+
+                depotKeysCache.Add(uint.Parse(split[0]), StringToByteArray(split[1]));
+            }
+        }
+
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+
+        public static bool ContainsKey(uint depotId)
+        {
+            return depotKeysCache.ContainsKey(depotId);
+        }
+
+        public static byte[] Get(uint depotId)
+        {
+            return depotKeysCache[depotId];
+        }
+
+
+    }
+}
